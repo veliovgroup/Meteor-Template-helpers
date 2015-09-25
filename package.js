ce9410046ba3c3c48bad12990f7416907704676c
Dmitriy A. Golev <dr.dimitru@gmail.com>
2015-09-25 22:18:50 +0300

    publish

--- package.js
+++ package.js
@@ -8,6 +8,6 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.use(['templating', 'underscore', 'ostrio:jsextensions'], 'client');
+  api.use(['templating', 'underscore'], 'client');
   api.addFiles('ostrio:templatehelpers.js', 'client');
 });
