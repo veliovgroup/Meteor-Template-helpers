b6bb885247b4400f78a07cd5cf92b0a05d9aa3f2
dr.dimitru <ceo@veliov.com>
2017-05-24 06:11:05 +0300

    v2.1.0
     - Fix #4 , via #5 , thanks to @dhrubins
     - `session` and `underscore` now is a weak dependencies
     - 100% tests coverage
     - Overall package fixes and enhancements

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ostrio:templatehelpers',
-  version: '2.0.4',
+  version: '2.1.0',
   summary: 'Template helpers for logical operations, variable comparison, Session, underscore and debugging',
   git: 'https://github.com/VeliovGroup/Meteor-Template-helpers',
   documentation: 'README.md'
@@ -8,7 +8,12 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.4');
-  api.use(['templating', 'spacebars', 'underscore', 'ecmascript'], 'client');
-  api.use('session', 'client', {weak: true});
+  api.use(['templating', 'spacebars', 'ecmascript'], 'client');
+  api.use(['session', 'underscore'], 'client', {weak: true});
   api.mainModule('templatehelpers.js', 'client');
+});
+
+Package.onTest(function(api) {
+  api.use(['ecmascript', 'tinytest', 'underscore', 'session', 'ostrio:templatehelpers'], 'client');
+  api.addFiles('templatehelpers-tests.js', 'client');
 });
