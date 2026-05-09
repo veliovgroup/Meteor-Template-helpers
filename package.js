b5f61449e38e5366ea46314ef8ae3f8ca6affdc0
dr.dimitru <ceo@veliov.com>
2026-05-09 22:56:51 +0300

    📦 v2.3.0

--- package.js
+++ package.js
@@ -1,19 +1,19 @@
 Package.describe({
   name: 'ostrio:templatehelpers',
-  version: '2.2.4',
+  version: '2.3.0',
   summary: 'Template helpers for logical operations, variable comparison, Session, underscore and debugging',
   git: 'https://github.com/VeliovGroup/Meteor-Template-helpers',
   documentation: 'README.md'
 });
 
 Package.onUse((api) => {
-  api.versionsFrom(['1.4', '2.0.1', '3.0.1']);
+  api.versionsFrom(['1.4', '2.0.1', '3.0.1', '3.4']);
   api.use(['templating', 'ecmascript'], 'client');
   api.use(['session', 'underscore'], 'client', {weak: true});
   api.mainModule('templatehelpers.js', 'client');
 });
 
 Package.onTest((api) => {
-  api.use(['templating', 'ecmascript', 'tinytest', 'underscore', 'session'], 'client');
+  api.use(['templating', 'templating-runtime', 'ecmascript', 'tinytest', 'underscore', 'session'], 'client');
   api.addFiles('templatehelpers-tests.js', 'client');
 });
